b0b7148b985681bd5b4c927056e3d24dc0d7bf03
abei2017 <abei@nai8.me>
2017-11-03 13:53:35 +0800

    初始化client代码

---
+++ client/app.js
@@ -0,0 +1,12 @@
+//app.js
+App({
+    onLaunch: function (options) {
+
+    },
+    onShow: function (options) {
+        
+    },
+    globalData: {
+        remoteUrl: 'http://xgh.nai8.me/app'
+    }
+})
