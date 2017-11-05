08752ec0d515b88fad5bb6164031a55ff493db06
abei2017 <abei@nai8.me>
2017-11-05 16:34:11 +0800

    完成小程序中相册列表的上拉加载和下拉刷新功能

--- client/pages/album/list.js
+++ client/pages/album/list.js
@@ -1,5 +1,8 @@
 // pages/album/list.js
 var app = getApp();
+var page = 1;
+var hadLastPage = false;
+
 Page({
 
     /**
@@ -20,23 +23,7 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-        var that = this;
-        wx.request({
-            method: 'GET',
-            url: 'http://xgh.local.com/xcx/albums',
-            data: {
-                fields: 'id,name',
-                expand: 'formatUpdateTime,photoTotal'
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success: function (res) {
-                that.setData({
-                    albums: res.data
-                });
-            },
-        })
+        this.loadList();
     },
 
     /**
@@ -64,14 +51,21 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
+        page = 1;
+        hadLastPage = false;
+        this.setData({
+            albums: []
+        });
+        this.loadList();
 
+        wx.stopPullDownRefresh();
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        this.loadList();
     },
 
     /**
@@ -79,5 +73,44 @@ Page({
      */
     onShareAppMessage: function () {
 
+    },
+
+    loadList:function(){
+        if (hadLastPage != false) {
+            wx.showToast({
+                title: '到底了',
+            })
+            return;
+        }
+
+        var that = this;
+        wx.request({
+            method: 'GET',
+            url: 'http://xgh.local.com/xcx/albums',
+            data: {
+                fields: 'id,name',
+                expand: 'formatUpdateTime,photoTotal',
+                page:page
+            },
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function (res) {
+                var listData = that.data.albums;
+                for (var i = 0; i < res.data.length; i++) {
+                    listData.push(res.data[i]);
+                }
+
+                if (res.header["X-Pagination-Page-Count"] == res.header["X-Pagination-Current-Page"]) {
+                    hadLastPage = res.header["X-Pagination-Current-Page"];
+                } else {
+                    page++;
+                }
+
+                that.setData({
+                    albums: listData
+                });
+            },
+        })
     }
 })
